d3a24f202dcfe31614f23149e3c1e82e8b9979c0
Enzo <88534255+enzovga@users.noreply.github.com>
2023-03-30 00:14:02 -0300

    Add files via upload

---
+++ ex7.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Enzo Vega Grozdas - AED2 - 2AN - Ex 7
+
+*******************************************************************************/
+#include <stdio.h>
+#include <math.h>
+
+double cubo(double num)
+{
+    return pow(num, 3);
+}
+
+void main()
+{
+    double num;
+    printf("Digite o valor que deseja retornar ao cubo: ");
+    scanf("%lf", &num);
+    printf("O cubo do numero %.2lf é %.2lf",num, cubo(num));
+}
+
+
