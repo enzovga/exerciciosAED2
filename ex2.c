1e5d406dc54cac515b02abc06e6f1af4ea06f914
Enzo <88534255+enzovga@users.noreply.github.com>
2023-04-05 19:49:31 -0300

    Atualizar o ex2.c

--- ex2.c
+++ ex2.c
@@ -5,7 +5,7 @@ Enzo Vega Grozdas - AED2 - 2AN - Ex 2
 *******************************************************************************/
 #include <stdio.h>
 
-int divisao(int n1, int n2)
+double divisao(int n1, int n2)
 {
     return (n1 / n2);
 }
